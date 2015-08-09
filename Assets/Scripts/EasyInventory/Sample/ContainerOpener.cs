05bb594e6d10314952510e06a08dda454cf945ef
Adam Passey <adamjpassey@gmail.com>
2015-08-08 17:30:47 -0700

    Creating sample `ContainerParent`
    * Sample shows easiest implementation of prefab containers
    * `CanvasHelper` created that will retrieve `Canvas` _by name_

---
+++ Assets/Scripts/EasyInventory/Sample/ContainerOpener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using EasyInventory.Containers;
+
+public class ContainerOpener : MonoBehaviour {
+
+    private ContainerParent containerParent;
+
+	// Use this for initialization
+	void Start () {
+        containerParent = GetComponent<ContainerParent>();
+	}
+	
+	public void OnMouseDown() {
+        containerParent.Container.Toggle();
+    }
+}
---
+++ Assets/Scripts/EasyInventory/Utils/CanvasHelper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EasyInventory.Utils {
+
+    public class CanvasHelper {
+
+        private static Canvas CurrentCanvas;
+        private static readonly string canvasName = "Canvas";
+
+        public static Canvas GetCanvas() {
+            if (CurrentCanvas != null) {
+                return CurrentCanvas;
+            }
+
+            GameObject canvasObject = GameObject.Find(canvasName);
+            CurrentCanvas = canvasObject.GetComponent<Canvas>();
+            return CurrentCanvas;
+        }
+    }
+}
